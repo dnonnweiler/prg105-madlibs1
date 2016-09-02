c30f6bfd8421fd63a7a644440278035e03aeb154
dnonnweiler <dnonnweiler@students.mchenry.edu>
2016-09-01 22:08:32 -0500

    Create dnonnweilerpoem.py

---
+++ dnonnweilerpoem.py
@@ -0,0 +1,39 @@
+#variables
+person = "Ben-Hur "
+vehicle = " chariot"
+rank = " Consul "
+person2 = "Quintus"
+hat = " helm"
+thick = " Macedonian pirates"
+handy = " romans "
+music = " spears "
+
+
+
+
+
+
+
+
+
+print (person + "came to town,")
+print ("A-ridin on a" + vehicle)
+print ("He stuck a feather in his" + hat)
+print ("And called it macaroni.")
+
+
+print (person + "keep it up,")
+print (person + "Dandy;")
+print ("Mind the" + music + "and the steps")
+print ("And with the" + handy + "be handy.")
+
+print ("Father and I went down to camp,")
+print ("Along with" + rank + person2)
+print ("The men and boys all stood around")
+print ("As thick as" + thick)
+
+
+print (person + "keep it up,")
+print (person + "Dandy;")
+print ("Mind the" + music + "and the steps")
+print ("And with the" + handy + "be handy.")
